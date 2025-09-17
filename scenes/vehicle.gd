8e62cce1af3846ba1fa1cb6e028a492956c29534
omriweinmann <omri.weinmann@gmail.com>
2025-09-16 19:06:33 -0700

    Trucks moving WOo and its GOOD no BUGS i think

--- scenes/vehicle.gd
+++ scenes/vehicle.gd
@@ -45,35 +45,24 @@ func _move(end_goal) -> void:
 	for x in path:
 		var add = "0000"
 		var offset = Vector2(-16,-10)
-		print(x - my_position)
+		#print(x - my_position)
 		var x_minus = x - my_position
 		if x_minus == Vector2i(-1,0):
 			add = "0100"
-			offset = Vector2(-8,-5)
+			#offset = Vector2(-8,-5)
 		if x_minus == Vector2i(0,-1):
 			add = "1000"
-			offset = Vector2(8,-5)
+			#offset = Vector2(8,-5)
 		if x_minus == Vector2i(1,0):
 			add = "0010"
-			offset = Vector2(8,5)
+			#offset = Vector2(8,5)
 		if x_minus == Vector2i(0,1):
 			add = "0001"
-			offset = Vector2(-8,5)
-		if not before_offset == Vector2(-1,-1) and not before_x == Vector2i(-1,-1):
-			if not before_offset == offset:
-				if (before_minus - x_minus) == Vector2i(-1,-1) or (before_minus - x_minus) == Vector2i(1,1):
-					var tween2 = create_tween()
-					#print(Global.map_to_local[x])
-					tween2.tween_property($".", "position", Global.map_to_local[before_x][0]+offset, 0)
-					$Sprite2D.texture = _load(_get_source()[2] + add + _get_source()[3])
-					await tween2.finished 
-				else:
-					var tween2 = create_tween()
-					#print(Global.map_to_local[x])
-					tween2.tween_property($".", "position", Global.map_to_local[before_x][0]+offset, 0.5)
-					await get_tree().create_timer(0.25, true, true).timeout
-					$Sprite2D.texture = _load(_get_source()[2] + add + _get_source()[3])
-					await tween2.finished 
+			#offset = Vector2(-8,5)
+		offset = Vector2(0,0)
+		if not before_minus == x_minus:
+			await get_tree().create_timer(0.125, true, true).timeout
+			$Sprite2D.texture = _load(_get_source()[2] + add + _get_source()[3])
 		#print(_get_source()[2] + add + _get_source()[3])
 		$Sprite2D.texture = _load(_get_source()[2] + add + _get_source()[3])
 		var tween = create_tween()
@@ -101,10 +90,10 @@ func _get_warehouse_vehicle():
 func _scary_pathfinding(start_miot:Vector2i, end_miot:Vector2i):
 	#await get_tree().create_timer(10, true, true).timeout
 	var open = {
-		Vector2i(-10,-10): [
+		Vector2i(-99999,-99999): [
 			Vector2i(-1,-1), 
-			(Vector2i(-10,-10) - end_miot).length()+(Vector2i(-10,-10) - start_miot).length(),
-			(Vector2i(-10,-10) - start_miot).length()
+			(Vector2i(-99999,-99999) - end_miot).length()+(Vector2i(-99999,-99999) - start_miot).length(),
+			(Vector2i(-99999,-99999) - start_miot).length()
 		],
 		start_miot: [
 			Vector2i(-1,-1), 
@@ -114,7 +103,7 @@ func _scary_pathfinding(start_miot:Vector2i, end_miot:Vector2i):
 	}
 	var closed = {}
 	while true:
-		var current = Vector2i(-10,-10)
+		var current = Vector2i(-99999,-99999)
 		var current_f_cost = 99999999
 		#print(current)
 		#print("open: ",open)
@@ -125,7 +114,7 @@ func _scary_pathfinding(start_miot:Vector2i, end_miot:Vector2i):
 				current = o
 				current_f_cost = open_o[1]
 				
-		if current == Vector2i(-10,-10):
+		if current == Vector2i(-99999,-99999):
 			#print(closed)
 			break #failsafe
 		
@@ -146,13 +135,26 @@ func _scary_pathfinding(start_miot:Vector2i, end_miot:Vector2i):
 				
 			return path
 		
-		var neighbors = [current+Vector2i(0,1),current+Vector2i(1,0),current+Vector2i(0,-1),current+Vector2i(-1,0)]
+		var neighbors:Array
+		if Global._get_building_info_of_v2i(current)[1] == 4:
+			neighbors = _get_2nr_neighbors(current)
+			#print(neighbors)
+		else:
+			neighbors = [current+Vector2i(0,1),current+Vector2i(1,0),current+Vector2i(0,-1),current+Vector2i(-1,0)]
+		for neighbor:Vector2i in neighbors:
+			#if Global._get_building_info_of_v2i(current)[1] == 4:
+				#print(neighbor)
+			var gbiov = Global._get_building_info_of_v2i(neighbor)
+			if not gbiov == []:
+				if gbiov[1] == 4:
+					if _get_2nr_neighbors(neighbor).find(current) == -1:
+						neighbors.erase(neighbor)
 		for neighbor:Vector2i in neighbors:
 			#print(Global.map_to_local.get(neighbor,[]))
 			if not Global.map_to_local.get(neighbor,[]) == []: #check if its on the map
 				#print("1")
 				#print(Global._get_building_info_of_v2i(neighbor),"uyg")
-				if Global.map_to_local[neighbor][1] and closed.get(neighbor,[]) == [] and (not Global._get_building_info_of_v2i(neighbor) == [] and not Global.road_changers.find(Global._get_building_info_of_v2i(neighbor)[1]) == -1): #check if its viable and if its not closed
+				if Global.map_to_local[neighbor][1] and closed.get(neighbor,[]) == [] and (not Global._get_building_info_of_v2i(neighbor) == [] and (not Global.road_changers.find(Global._get_building_info_of_v2i(neighbor)[1]) == -1 or Global._get_building_info_of_v2i(neighbor)[1] == 4)): #check if its viable and if its not closed
 					#print("2")
 					if (not open.get(neighbor,[]) == [] and closed[current][2] + (current-neighbor).length() < open.get(neighbor,[])[2]) or open.get(neighbor,[]) == []:
 						#print("3")
@@ -169,3 +171,21 @@ func _scary_pathfinding(start_miot:Vector2i, end_miot:Vector2i):
 	
 func _process(_delta: float) -> void:
 	$Sprite2D.position = Vector2(0,-Global.map_to_local[my_position][2])
+
+
+func _get_2nr_neighbors(v2i:Vector2i):
+	var direction_str = _get_direction_string(v2i)
+	print(direction_str)
+	if direction_str == "0":
+		return [v2i+Vector2i(-1,0),v2i+Vector2i(1,0)]
+	elif direction_str == "1":
+		return [v2i+Vector2i(0,-1),v2i+Vector2i(0,1)]
+	return []
+
+func _get_direction_string(v2i:Vector2i):
+	var array_location = Global.built.find(v2i)
+	if not array_location == -1:
+		var array = Global.built_data[array_location]
+		var direction = array[2]
+		return str(direction)
+	return ""
